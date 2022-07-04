2dc713f086727e6b9780909a23fa9f1a1ea2d0e6
faitsse <2138889191@qq.com>
2022-07-04 11:14:46 +0800

    chore: update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+import { replacePrimaryColor } from '~/utils'
 
+useHead({
+  title: 'Parfait',
+  meta: [
+    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: computed(() => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg'),
+    },
+  ],
+})
+
+// reset primary color
+const { primaryColor } = storeToRefs(useAppStore())
+replacePrimaryColor(primaryColor.value)
+const { appReloading, handleReload } = useReload()
+handleReload(1500)
 </script>
 
 <template>
-  Hello
+  <WaitForWindowReload v-if="appReloading" />
+  <RouterView v-else />
 </template>
-
-<style>
-
-</style>
