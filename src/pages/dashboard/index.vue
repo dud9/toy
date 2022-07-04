a32476270787cb693be55424f130058a4eef0af5
faitsse <2138889191@qq.com>
2022-07-04 16:10:53 +0800

    chore: update

--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -6,12 +6,27 @@ function queryMenus() {
     permissionStore.fetchAppMenus()
 }
 queryMenus()
+const equipName = '数控拼焊'
 </script>
 
 <template>
-  <div flex="~ col" h-full w="4/5" mx="1/10" pt-12>
-    <div h-100px w-full border="1 red">
-      22
+  <div flex="~ col" w="4/5" mx="1/10" pt-12 font-bold>
+    <div
+      h-100px min-h-50px w-full
+      border="5 [var(--color-primary-light-2)]"
+      rounded-3 grid="~ flow-row-dense cols-3"
+      text="1.2rem" lg="text-1.8rem"
+      md="text-1.5rem" xl="text-2.1rem"
+    >
+      <div border="1 red" h-full flex justify-center items-center>
+        {{ equipName }}
+      </div>
+      <div h-full flex justify-center items-center>
+        <span mr-2>设备</span> 在线
+      </div>
+      <div h-full flex justify-center items-center>
+        <span mr-2>边缘端</span> 在线
+      </div>
     </div>
     <div min-h-300px w-full mt-8 border="1 red">
       11
