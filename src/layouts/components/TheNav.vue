4c02361dd5957c82dd488c32dd61d52ab390b33b
faitsse <2138889191@qq.com>
2022-07-04 14:22:53 +0800

    chore: update

--- src/layouts/components/TheNav.vue
+++ src/layouts/components/TheNav.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import NavBreadCrumb from '../widgets/NavBreadCrumb.vue'
 import NavFullScreen from '../widgets/NavFullScreen.vue'
 import NavStyleSetting from '../widgets/NavStyleSetting.vue'
 import NavAvatar from '../widgets/NavAvatar.vue'
@@ -7,25 +6,19 @@ import Logo from '../widgets/Logo.vue'
 import Menu from '../widgets/Menu.vue'
 
 const { appMenus } = storeToRefs(usePermissionStore())
-const mode: 'vertical' | 'horizontal' | 'pop' | 'popButton' = 'horizontal'
-const { layout } = storeToRefs(useAppStore())
-const isVertical = computed(() => {
-  return layout.value === 'vertical'
-})
+
 const { width } = useWindowSize()
 let shortLogo = $ref(false)
 watchEffect(() => {
-  if (!isVertical.value)
-    shortLogo = width.value < 1200
+  shortLogo = width.value < 1200
 })
-const rightPanelVisible = ref(false)
+const navStyleSettingPanelVisible = ref(false)
 </script>
 
 <template>
   <div flex justify-between items-center pr-5 shadow="sm light-900 dark:dark-700">
-    <Logo v-if="!isVertical" :class="shortLogo ? 'w-64px' : 'w-200px'" />
-    <NavBreadCrumb v-if="isVertical" mr-4 />
-    <Menu v-if="!isVertical" :mode="mode" :metadata="appMenus" />
+    <Logo :class="shortLogo ? 'w-64px' : 'w-200px'" />
+    <Menu mode="horizontal" :metadata="appMenus" />
     <div flex-auto />
     <a
       icon-btn text-lg i-carbon-logo-github mx-3
@@ -34,7 +27,7 @@ const rightPanelVisible = ref(false)
     />
     <NavFullScreen mr-3 />
     <DarkToggle mr-3 />
-    <NavStyleSetting v-model:visible="rightPanelVisible" />
+    <NavStyleSetting v-model:visible="navStyleSettingPanelVisible" />
     <NavAvatar mx-3 />
   </div>
 </template>
