3b114639a237bf021e02326062b5bdd064bea82a
faitsse <2138889191@qq.com>
2022-07-05 17:52:48 +0800

    chore: update

--- src/components/WaitForWindowReload.vue
+++ src/components/WaitForWindowReload.vue
@@ -3,9 +3,9 @@
     h-screen w-screen z-10000 fixed top-0 left-0
     bg="white dark:#17171A" flex="~ col" justify-center items-center
   >
-    <div i-carbon-logo-pinterest class="bg-[rgb(var(--primary-6))]" text-80px />
-    <a-spin dot :size="24" class="mt-1/10" />
-    <div text-36px mt-120px font-bold>
+    <div i-carbon-campsite class="bg-[rgb(var(--primary-6))]" text-100px />
+    <a-spin dot :size="24" class="mt-1/12" />
+    <div text-36px mt-100px font-bold>
       TOY
     </div>
   </div>
