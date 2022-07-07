7cffdd1aa61301758c262671b76494503a50684b
faitsse <2138889191@qq.com>
2022-07-07 14:40:22 +0800

    chroe: update

--- src/server/api/auth.ts
+++ src/server/api/auth.ts
@@ -2,4 +2,8 @@ export class AuthApi {
   static login(params: Record<string, any>) {
     return useRequest.get('/authApi/login', { urlAdd: params })
   }
+
+  static updatePassword(params: Record<string, any>) {
+    return useRequest.post('/authApi/password', { body: params })
+  }
 }
