8e8e63c9b303e6279f20110af103543c05095547
faitsse <2138889191@qq.com>
2022-07-07 20:38:08 +0800

    chore: update

--- src/pages/react-data/components/ReactDataList.vue
+++ src/pages/react-data/components/ReactDataList.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <div w-full h-full flex="~ col" justify-center items-center>
-    <div v-for="i, idx in 10" :key="idx" flex="~ wrap">
-      头座模块自检报警{{ i }}: 离线
+  <div w-full h-full flex="~ col" justify-start items-center pt-10px of="x-hidden y-auto">
+    <div v-for="i, idx in 10" :key="idx" flex="~ wrap" mt-4 font-bold>
+      <span>头座模块自检报警{{ i }}</span>: 离线
     </div>
   </div>
 </template>
