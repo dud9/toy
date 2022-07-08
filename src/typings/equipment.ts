b52b22c5f8667ae1607a0ad39f0dc62b159578fb
faitsse <2138889191@qq.com>
2022-07-08 11:08:58 +0800

    chore: update

---
+++ src/typings/equipment.ts
@@ -0,0 +1,10 @@
+export interface Equipment {
+  id: number
+  name?: string
+  type?: string
+  productionDate?: Date
+  information?: string
+  ip?: string
+  port?: number
+  collectInterval?: number
+}
