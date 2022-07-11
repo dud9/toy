2de5e61ca9e144b6bbf7c5602ec3abf598b210ce
faitsse <2138889191@qq.com>
2022-07-11 14:35:48 +0800

    chore: update

--- src/pages/history-data/index.vue
+++ src/pages/history-data/index.vue
@@ -7,7 +7,7 @@ import HistoryDataChart from './components/HistoryDataChart.vue'
     <PageHeader />
     <div
       style="height: calc(100vh - 140px);"
-      w-full flex="inline nowrap" lt-md="grid grid-rows-2"
+      w-full flex="inline nowrap"
       border="1 neutral-600/10 dark:![var(--color-neutral-3)]"
     >
       <HistoryDataChart />
