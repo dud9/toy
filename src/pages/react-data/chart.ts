c49b86c9e11aa8cd9dcb4eb491bb980fab79fef3
faitsse <2138889191@qq.com>
2022-07-12 17:06:16 +0800

    chore: update

--- src/pages/react-data/chart.ts
+++ src/pages/react-data/chart.ts
@@ -1,33 +1,5 @@
 type EChartsOption = echarts.EChartsOption
 
-export interface DataItem {
-  name: string
-  value: [string, number]
-}
-
-let now = new Date(1997, 9, 3)
-const oneDay = 24 * 3600 * 1000
-let value = Math.random() * 1000
-
-export function randomData(): DataItem {
-  now = new Date(+now + oneDay)
-  value = value + Math.random() * 21 - 10
-  return {
-    name: now.toString(),
-    value: [
-      [now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'),
-      Math.round(value),
-    ],
-  }
-}
-
-export function generateChartData() {
-  const data: DataItem[] = []
-  for (let i = 0; i < 1000; i++)
-    data.push(randomData())
-  return data
-}
-
 export const defaultOption: EChartsOption = {
   title: {
     show: false,
@@ -45,19 +17,6 @@ export const defaultOption: EChartsOption = {
       color: unref(isDark) ? '#fff' : '#333',
     },
     trigger: 'axis',
-    formatter(params: any) {
-      params = params[0]
-      const date = new Date(params.name)
-      return (
-        `${date.getDate()
-        }/${
-        date.getMonth() + 1
-        }/${
-        date.getFullYear()
-        } : ${
-        params.value[1]}`
-      )
-    },
     axisPointer: {
       animation: false,
     },
